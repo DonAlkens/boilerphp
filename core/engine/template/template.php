74e94103957f4e3d762836c9718f04185be7c4e0
donalkens <akinniyiakinpelumi@gmail.com>
2019-04-13 01:16:57 +0100

    added layout extends syntax and fucntionality

--- core/engine/template/template.php
+++ core/engine/template/template.php
@@ -25,9 +25,25 @@ class TemplateEngine {
     }
 
 
-    public function extendFile($fcontent, $content=null) {
+    public function extendLayout($fcontent, $viewPath, $ext) {
 
-        $fcontent = preg_replace("/\{\% extends (.*) \%\}/", '<?php } ?>',$fcontent);
+        if(str_replace("@{extends","",$fcontent)) {
+
+            $layout = substr($fcontent, strpos($fcontent,"@{extends "), strpos($fcontent, "}"));
+            $layout = str_replace("@{extends \"","",$layout);
+            $cleaned = str_replace("\"","",trim($layout," "));
+            $layoutPath = $viewPath."/".$cleaned."."."$ext";
+
+            $layout = file_get_contents($layoutPath);
+
+            $fcontent = preg_replace("/@\{extends (.*)\}/","",$fcontent);
+            
+            $layout = preg_replace("/@\{(.*)content(.*)\}/", $fcontent, $layout);
+            
+            return $layout;
+        }
+
+        return $fcontent;
   
     }
 
@@ -114,70 +130,23 @@ class TemplateEngine {
                 foreach($content[("$1")] as $key => $value) { ?>'
         ,$fcontent);
 
-        $fcontent = preg_replace("/\{\{ key \}\}/", 
-            '<?php
-                echo $key; 
-            ?>'
-        ,$fcontent);
-        
-        $fcontent = preg_replace("/\{\{ value \}\}/", 
-            '<?php
-                echo $value; 
-            ?>'
-        ,$fcontent);
-
-        $fcontent = preg_replace("/\{\% endforeach \%\}/", 
-            '<?php
-                } }
-            ?>'
-        ,$fcontent);
+        $fcontent = preg_replace("/\{\{ key \}\}/", '<?php echo $key; ?>',$fcontent);
+        $fcontent = preg_replace("/\{\{ value \}\}/", '<?php echo $value; ?>',$fcontent);
+        $fcontent = preg_replace("/\{\% endforeach \%\}/", '<?php } } ?>',$fcontent);
 
         $fcontent = preg_replace("/\{\% loop (.*) \%\}/",
             '<?php  if(array_key_exists("$1", $content)) {
                     for($i = 0; $i < count($content["$1"]); $i++) { ?>'
         ,$fcontent);
         
-        $fcontent = preg_replace("/\{\{ \[index\+\+\] \}\}/", 
-            '<?php echo $i + 1; ?>'
-        ,$fcontent);
-
-        $fcontent = preg_replace("/\{\{ \[index\] \}\}/", 
-            '<?php echo $i; ?>'
-        ,$fcontent);
-
-        $fcontent = preg_replace("/\{\{ (.*)\[index\] \}\}(.*)\{\{/", 
-            '<?php echo $content["$1"][$i]; ?>$2{{'
-        ,$fcontent);
-
-        $fcontent = preg_replace("/\{\{ (.*)\[index\]\.(.*) \}\}(.*)\{\{/", 
-            '<?php
-                echo $content["$1"][$i]["$2"];
-            ?>$3{{'
-        ,$fcontent);
-        
-        $fcontent = preg_replace("/\{\{ (.*)\[index\]\.(.*) \}\}(.*)\{/", 
-            '<?php
-                echo $content["$1"][$i]["$2"];
-            ?>/{{'
-        ,$fcontent);
-
-        $fcontent = preg_replace("/\{\{ (.*)\[index\]\.(.*) \}\}/", 
-            '<?php
-                echo $content["$1"][$i]["$2"];
-            ?>'
-        ,$fcontent);
-        
-                
-        $fcontent = preg_replace("/\{\{ (.*)\[index\] \}\}/", 
-            '<?php echo $content["$1"][$i]; ?>'
-        ,$fcontent);
-                        
-
-        $fcontent = preg_replace("/\{\% endloop \%\}/", 
-            '<?php
-                } }
-            ?>'
-        ,$fcontent);
+        $fcontent = preg_replace("/\{\{ \[index\+\+\] \}\}/", '<?php echo $i + 1; ?>',$fcontent);
+        $fcontent = preg_replace("/\{\{ \[index\] \}\}/",'<?php echo $i; ?>',$fcontent);
+        $fcontent = preg_replace("/\{\{ (.*)\[index\] \}\}(.*)\{\{/", '<?php echo $content["$1"][$i]; ?>$2{{',$fcontent);
+        $fcontent = preg_replace("/\{\{ (.*)\[index\]\.(.*) \}\}(.*)\{\{/", '<?php echo $content["$1"][$i]["$2"]; ?>$3{{',$fcontent);
+        $fcontent = preg_replace("/\{\{ (.*)\[index\]\.(.*) \}\}(.*)\{/", '<?php echo $content["$1"][$i]["$2"]; ?>/{{',$fcontent);
+        $fcontent = preg_replace("/\{\{ (.*)\[index\]\.(.*) \}\}/", '<?php echo $content["$1"][$i]["$2"]; ?>',$fcontent);    
+        $fcontent = preg_replace("/\{\{ (.*)\[index\] \}\}/",'<?php echo $content["$1"][$i]; ?>',$fcontent);
+        $fcontent = preg_replace("/\{\% endloop \%\}/", '<?php } } ?>',$fcontent);
 
         
         return $fcontent;
@@ -185,7 +154,6 @@ class TemplateEngine {
     }
 
     static function sessions($fcontent){
-        
         
         foreach($_SESSION as $key => $value) {
 
