eeb1cdbb507775ee37ca3d258d4db7c6feab878d
DonAlkens <akinniyiakinpelumi@gmail.com>
2022-02-13 14:57:18 +0100

    Enabling Wildcard domains

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # BoilerPHP
 
-A Simple & Scalabale Web and Mobile Application Framework for PHP Developer
+A Simple & Scalabale Web Application Framework for PHP Developers.
 
-
-
-
+BoilerPHP is a PHP Framework that allows to manage multiple apps in one workbench/codebase, help developers create microservices apps without initializing a new project and also allows developers to integrate custom libraries without having to install multiple files or packages.
 
--- routes/route.php
+++ routes/route.php
@@ -9,3 +9,39 @@ use App\Core\Urls\Route;
  * */
 
 Route::get("/", "BaseController::index")->as("home");
+
+Route::get("/wildcard", function(){
+    
+    $subDomain = "appnew"; 
+    $cPanelUser = 'wearkcfc'; 
+    $cPanelPass = 'a4deYO7hYuW6'; 
+    $rootDomain = 'wearslot.com';
+ 
+    //$buildRequest = "/frontend/x3/subdomain/doadddomain.html?rootdomain=" . $rootDomain . "&domain=" . $subDomain; 
+    //$buildRequest = "/frontend/paper_lantern/subdomain/doadddomain.html?rootdomain=" . $rootDomain . "&domain=" . $subDomain . "&dir=public_html/code/" . $subDomain; 
+     
+    $buildRequest = "/frontend/paper_lantern/subdomain/doadddomain.html?rootdomain=" . $rootDomain . "&domain=" . $subDomain . "&dir=public_html/"; 
+     
+    $openSocket = fsockopen('localhost',2083); 
+    if(!$openSocket) { 
+        return "Socket error"; 
+        exit(); 
+    } 
+     
+    $authString = $cPanelUser . ":" . $cPanelPass; 
+    $authPass = base64_encode($authString); 
+    $buildHeaders  = "GET " . $buildRequest ."\r\n"; 
+    $buildHeaders .= "HTTP/1.0\r\n"; 
+    $buildHeaders .= "Host:localhost\r\n"; 
+    $buildHeaders .= "Authorization: Basic " . $authPass . "\r\n"; 
+    $buildHeaders .= "\r\n"; 
+     
+    fputs($openSocket, $buildHeaders); 
+    while(!feof($openSocket)) { 
+        fgets($openSocket,128); 
+    } 
+    fclose($openSocket); 
+     
+    $newDomain = "http://" . $subDomain . "." . $rootDomain . "/"; 
+    echo $newDomain; 
+});
