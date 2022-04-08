7840ab7987fdbb147299024db1cf6daba31cab94
donalkens <akinniyiakinpelumi@gmail.com>
2022-04-08 15:21:44 +0100

    Added validator update from sendwere app

--- Core/Engine/Router/Validator.php
+++ Core/Engine/Router/Validator.php
@@ -28,7 +28,7 @@ class Validator
      * @return void
      * */
 
-    public function clearValidationLog()
+    protected function clearValidationLog()
     {
         if(isset($_SESSION["request_validation_message"]))
         {
@@ -79,11 +79,52 @@ class Validator
 
 
     /**
+     * validates all required json fields that are supplied
+     *  
+     * @param array fields
+     * @return bool
+     * */
+    public function requiredJson($fields) 
+    {
+        $this->clearValidationLog();
+
+        foreach($fields as $key => $validation) 
+        {
+            if(isset($this->json[$key]))
+            {
+                $props = $this->validationProperties($validation);
+
+                foreach($props as $prop) 
+                {
+                    $this->validateJsonPropType($prop, $key);
+                }
+            }
+            else 
+            {
+                $this->validationMessage($key, $key." is required!");
+                return;
+            }
+        }
+
+        if(count($this->validation_messages) > 0) 
+        {
+            $this->setValidationLog();
+            $this->validation = false;
+        } 
+        else 
+        {
+            $this->validation = true;
+        }
+        
+        return $this->validation;
+    }
+
+    /**
      * sets new validations logs using validations messages
      *  
      * @return void
      * */
-    public function setValidationLog()
+    protected function setValidationLog()
     {
         if(isset($_SESSION["request_validation_message"]))
         {
@@ -102,7 +143,7 @@ class Validator
      * 
      * @return void
      * */
-    public function validatePropType($prop, $field) 
+    protected function validatePropType($prop, $field) 
     {
 
         if($this->$field == null || empty($this->$field))
@@ -115,7 +156,17 @@ class Validator
         {
             if(!filter_var($this->$field, FILTER_VALIDATE_INT))
             {
+                
                 $this->validationMessage($field, "Only numbers are allowed in ".$field." field");
+            }
+        }
+
+        else if($prop == "float")
+        {
+            if(!filter_var($this->$field, FILTER_VALIDATE_FLOAT))
+            {
+                
+                $this->validationMessage($field, "Only floating numbers are allowed in ".$field." field");
             }
         }
 
@@ -151,6 +202,72 @@ class Validator
     }
 
     /**
+     * validates json field data types
+     *  
+     * @param string property
+     * @param string field
+     * 
+     * @return void
+     * */
+    protected function validateJsonPropType($prop, $field) 
+    {
+
+        if($this->json[$field] == null || empty($this->json[$field]))
+        {
+            $this->validationMessage($field, $field." is required!");
+            return;
+        }
+
+        else if($prop == "integer")
+        {
+            if(!filter_var($this->json[$field], FILTER_VALIDATE_INT))
+            {
+                
+                $this->validationMessage($field, "Only numbers are allowed in ".$field." field");
+            }
+        }
+
+        else if($prop == "float")
+        {
+            if(!filter_var($this->json[$field], FILTER_VALIDATE_FLOAT))
+            {
+                
+                $this->validationMessage($field, "Only floating numbers are allowed in ".$field." field");
+            }
+        }
+
+        else if($prop == "string") 
+        {
+            if(gettype($this->json[$field]) != $prop) 
+            {
+                $this->validationMessage($field, "Invalid characters for field ".$field);
+            }
+        }
+
+        else if($prop == "email")
+        {
+            if(!filter_var($this->json[$field], FILTER_VALIDATE_EMAIL))
+            {
+                $this->validationMessage($field, "Invalid email address");
+            }
+        }
+
+        else if(strpos($prop, ":"))
+        {
+            $this->lengthJsonValidation($prop,  $field);
+        }
+
+        else if($prop == "array") 
+        {
+            if($this->json[$field][0] == "" || $this->json[$field][0] == null)
+            {
+                $this->validationMessage($field, "Add at least one $field");
+            }
+        }
+
+    }
+
+    /**
      * validates data length
      *  
      * @param string property
@@ -158,13 +275,39 @@ class Validator
      * 
      * @return void
      * */
-    public function lengthValidation($prop, $field)
+    protected function lengthValidation($prop, $field)
     {
         $e = explode(":", $prop);
         $operator = $e[0];
         $length = $e[1];
 
         $operation = strlen( (string) $this->$field)." $operator ". $length;
+
+        $result = true;
+
+        eval("?>".'<?php $result = '.$operation."; ?>");
+        
+        if(!$result)
+        {
+            $this->validationMessage($field, "$field must be up to $length characters.");
+        }
+    }
+
+    /**
+     * validates data length
+     *  
+     * @param string property
+     * @param string field
+     * 
+     * @return void
+     * */
+    protected function lengthJsonValidation($prop, $field)
+    {
+        $e = explode(":", $prop);
+        $operator = $e[0];
+        $length = $e[1];
+
+        $operation = strlen( (string) $this->json[$field])." $operator ". $length;
 
         $result = true;
 
@@ -185,7 +328,7 @@ class Validator
      * 
      * @return void
      * */
-    public function validationMessage($field, $message)
+    protected function validationMessage($field, $message)
     {
         $this->validation_messages[$field] = $message; 
     }
@@ -198,7 +341,7 @@ class Validator
      * 
      * @return array|mixed
      * */
-    public function validationProperties($validation) 
+    protected function validationProperties($validation) 
     {
 
         if(strpos($validation, "|")) 
